732f2e0d6a1b572f134310dcde3ee8ba85ff1117
userName <userEmail>
2024-09-09 00:19:09 +0800

    add requirements.txt

--- test.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import sessionmaker
-
-from database.api import get_reports, uploadImage
-from database.db import engine
-from utils import resolveAccountJwt,makeAccountJwt
-
-Session = sessionmaker(bind=engine)
-session = Session()
-print(uploadImage(session,"a.jpg",b'114514'))
